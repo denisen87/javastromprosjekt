cab571c8f7d307a83635ed051f1b0ceb2d7063b8
denisen87 <denisen87@hotmail.com>
2025-10-07 21:26:22 +0200

    oppgave 1

--- src/main/java/no/hvl/dat100/javel/oppgave1/DailyPower.java
+++ src/main/java/no/hvl/dat100/javel/oppgave1/DailyPower.java
@@ -4,59 +4,72 @@ public class DailyPower {
 
     // a) print power prices during a day
     public static void printPowerPrices(double[] prices) {
+        for(int i = 0; i < prices.length; i++) {
+            System.out.print(prices[i] + " kr ");
 
-        // TODO test
+         }
 
     }
 
     // b) print power usage during a day
-    public static void printPowerUsage(double[] usage) {
-
-        // TODO
+    public static void printPowerUsage(double[] poweruse) {
+            for(int i = 0; i < poweruse.length; i++) {
+                System.out.print(poweruse[i] + " kWh ");
+            }
 
     }
 
     // c) compute power usage for a single day
     public static double computePowerUsage(double[] usage) {
-
         double sum = 0;
-
-        // TODO
-
+        for (int i = 0; i < usage.length; i++) {
+            sum = sum + usage[i];
+        }
         return sum;
+
     }
 
     // d) compute spot price for a single day
     public static double computeSpotPrice(double[] usage, double[] prices) {
+        double sum = 0;
+        for (int i = 0; i < usage.length; i++) {
+            sum = sum + prices[i]*usage[i];
+        }
 
-        double price = 0;
-
-        // TODO
-
-        return price;
+        return sum;
     }
 
     // e) compute power support for a given usage and price
     private static final double THRESHOLD = 0.9375;
     private static final double PERCENTAGE = 0.9;
 
-    private static double getSupport(double usage, double price) {
-
+    public static double getSupport(double usage, double price) {
         double support = 0;
-
-        // TODO
-
+        if (price > 0.9375) {
+            support = (price-0.9375) * usage * 0.9;
+            System.out.print("støtten er på " + support);
+        } else {
+            System.out.println(" ingen støtte gis");
+        }
         return support;
     }
 
     // f) compute power support for a single day
     public static double computePowerSupport(double[] usage, double[] prices) {
 
-        double support = 0;
+        double totalsupport = 0;
+        for(int i = 0; i < prices.length; i++){
+            double price = prices[i];
+            double use = usage[i];
+            if (price > 0.9375) {
+                totalsupport = totalsupport + getSupport(use, price);
+            }
+            else{
+                totalsupport = totalsupport + 0 ;
 
-        // TODO
-
-        return support;
+            }
+        }
+        return totalsupport;
     }
 
     private static final double NORGESPRIS_KWH = 0.5;
@@ -64,29 +77,52 @@ public class DailyPower {
     // g) compute norges pris for a single day
     public static double computeNorgesPrice(double[] usage) {
 
-        double price = 0;
+        double totalforbruk = 0;
+        for (int i = 0; i < usage.length; i++) {
+            totalforbruk = usage[i] + totalforbruk; // henter verdien til plassen i og legger den sammen med total-
+            // forbruk, i neste sekvens er verdien til totalforbruk den samme verdien usage[i] hadde i forrige sekvens,
+            // usage[i] får en ny verdi i neste sekvens, som igjen legges sammen med verdien til totalforbruk
 
-        // TODO
+        }
 
-        return price;
+        return totalforbruk * NORGESPRIS_KWH;
+
     }
 
     // g) compute peak usage during a single day
     public static double findPeakUsage(double[] usage) {
 
-        double temp_max = 0;
+        double forbruk_max = 0; // max forbruk starter på 0
+        for (int i = 0; i < usage.length; i++) { // kjører løkken gjennom alle plassene i tabellen
+            double forbruk = usage[i]; // putter verdien i plassen i inn i variabelen forbruk
+            if (forbruk > forbruk_max) { // vist forbruk er høyere enn forbruk maks som er 0 i første omgang -
+                forbruk_max = forbruk; // så settes forbruks verdil til maks, da det er ingen høyere verdi enn
+                // verdien til forbruk,
+            }
+            else {
+                forbruk = forbruk; // ellers vist ikke forbruk > maksforbruk så har vi ikke en ny maks verdi, og
+                // setter derfor forbuk lik forbruk som da gjør at den høyeste verdien frem til no står uendret, denne
+                // linjer er ikke i bruk, men er ment som et bilde på hva som skjer med forbruk i bakrunnen
+            }
+        }
 
-        // TODO
+        return forbruk_max;
 
-        return temp_max;
     }
 
-    public static double findAvgPower(double[] usage) {
 
-        double average = 0;
+    public static double findAvgPower(double[] usage){
 
-        // TODO
+            double sum = 0;
+            double antall = 0;
+            for(int i = 0; i < usage.length; i++){
+                double forbruk = usage[i];
+                sum = sum + forbruk;
+                antall = antall + 1;
 
-        return average;
-    }
+            }
+            double gjennomsnitt = sum/antall;
+
+            return gjennomsnitt;
+        }
 }
--- src/main/java/no/hvl/dat100/javel/oppgave1/DayMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave1/DayMain.java
@@ -5,24 +5,47 @@ import no.hvl.dat100.javel.oppgave2.MonthlyPower;
 public class DayMain {
 
     public static void main(String[] args) {
-
-        // test data
-        double[] powerusage_day = DayPowerData.powerusage_day;
-
-        double[] powerprices_day = DayPowerData.powerprices_day;
-
-        System.out.println("==============");
-        System.out.println("OPPGAVE 1");
-        System.out.println("==============");
+        double[] prices = DayPowerData.powerprices_day; // putter inn verdiene i -
+        // tabellen inn variabelen prices
+        DailyPower.printPowerPrices(prices); // referer til klassen DayliPower hvor
+        // metoden printPowerPrices ligger, kjører så metoden printPowerPrices som
+        // henter ut verdiene fra variabelen prices
         System.out.println();
+        double [] poweruse = DayPowerData.powerusage_day;
+        DailyPower.printPowerUsage(poweruse);
+        double totalusage = DailyPower.computePowerUsage(poweruse); // putter verdien
+        // som metoden computePowerUsage(poweruse) lager inn variabelen totalusage,
+        // metoden computePowerUsage(poweruse) lager den sammenlagte verdien av all
+        // strømforbruket som er i powerusage_day, men printer den ikke ut,
+        System.out.printf("%nTotalt strømforbruk for dagen: %.2f kWh%n", totalusage);
+        // det gjør metoden System.out.print
+        double dagspris = DailyPower.computeSpotPrice(DayPowerData.powerusage_day,DayPowerData.powerprices_day);
+        // metoden computeSpotPrice går ned tabellene powerusage_day og powerprices_day og henter tabellverdiene,
+        // regner om slik at vi får dagsprisen på strømmen, putter denne i variabelen dagspris, skriver ut med println.
+        System.out.println("Prisen for strøm hele dagen er: " + dagspris + " kr");
+        double usage = 2.0;      // kWh
+        double price = 1.20;
+        double support1 = DailyPower.getSupport(2,1.2);
+        System.out.printf("Støtte ved pris %.2f kr", price, support1);
+        double sumstotte = DailyPower.computePowerSupport(DayPowerData.powerusage_day,DayPowerData.powerprices_day);
+        System.out.printf("Total støtte for dagen: %.4f kr%n", sumstotte);
+        double prisstrom = DailyPower.computeNorgesPrice(DayPowerData.powerusage_day);
+        System.out.printf("Pris med Norgespris (0.50 kr/kWh): %.2f kr%n", prisstrom);
+        double storsteforbruk = DailyPower.findPeakUsage(DayPowerData.powerusage_day);
+        System.out.printf("Største forbruk i løpet av dagen: %.2f kWh%n", storsteforbruk);
+        double gjennomsnittforbruk = DailyPower.findAvgPower(DayPowerData.powerusage_day);
+        System.out.printf(" gjennomsnittlig strømforbruk i løpet av dagen er : %.2f ", + gjennomsnittforbruk);
 
-        /*
-        TODO
 
-         Write code that tests the methods you implement in the DailyPower class
-         Remember to teste the methods as you implement them
-         Remember to also to check that you get the expected results
-         */
+
+
+
+
+         //
+         //Write code that tests the methods you implement in the DailyPower class
+         //Remember to teste the methods as you implement them
+         // Remember to also to check that you get the expected results
+
 
     }
 }
