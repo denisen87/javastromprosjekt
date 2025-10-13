615b2b701fadecb124ec027a35f487ee3d40b1c0
denisen87 <denisen87@hotmail.com>
2025-10-13 09:11:20 +0200

    Oppdatert MonthMain og MonthlyPower i oppgave2

--- src/main/java/no/hvl/dat100/javel/oppgave2/MonthMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave2/MonthMain.java
@@ -1,29 +1,25 @@
 package no.hvl.dat100.javel.oppgave2;
 
-import no.hvl.dat100.javel.oppgave1.DayPowerData;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class MonthMain {
     public static void main(String[] args) {
+        double[][] powerUsageMonth = MonthPowerData.powerusage_month;
+        double[][] powerPricesMonth = MonthPowerData.powerprices_month;
 
-        // test data
-        double[][] power_prices_month = MonthPowerData.powerprices_month;
+        System.out.println("Månedlig strømforbruk:");
+        MonthlyPower.printPowerUsage(powerUsageMonth);
 
-        double[][] power_usage_month = MonthPowerData.powerusage_month;
+        System.out.println("\nMånedlige strømpriser:");
+        MonthlyPower.printPowerPrices(powerPricesMonth);
 
-        System.out.println("==============");
-        System.out.println("OPPGAVE 2");
-        System.out.println("==============");
-        System.out.println();
+        double totalUsage = MonthlyPower.computePowerUsage(powerUsageMonth);
+        System.out.println("\nTotal månedlig forbruk: " + totalUsage + " kWh");
 
-        /*
-        TODO
+        boolean exceeded = MonthlyPower.exceedThreshold(powerUsageMonth, 1000);
+        System.out.println("Overskredet grense: " + exceeded);
 
-         Write code that tests the methods you implement in the MonthlyPower class
-         Remember to teste the methods as you implement them
-         Remember to also to check that you get the expected results
-         */
-
+        double monedspris = MonthlyPower.computeSpotPrice(powerUsageMonth, powerPricesMonth);
+        System.out.println("Totalt månedspris: " + monedspris + " kr");
     }
 }
--- src/main/java/no/hvl/dat100/javel/oppgave2/MonthlyPower.java
+++ src/main/java/no/hvl/dat100/javel/oppgave2/MonthlyPower.java
@@ -1,54 +1,77 @@
 package no.hvl.dat100.javel.oppgave2;
 
-import no.hvl.dat100.javel.oppgave1.DailyPower;
-
 public class MonthlyPower {
 
-    // a) print power usage for a month
-    public static void print_PowerUsage(double[][] usage) {
-
-        // TODO
-
+    public static void printPowerUsage(double[][] poweruse) {
+        for (double[] row : poweruse) {
+            for (double value : row) {
+                System.out.print(value + " kWh ");
+            }
+            System.out.println();
+        }
     }
 
-    // b) print power prices for a month
-    public static void print_PowerPrices(double[][] prices) {
-
-        // TODO
-
+    public static void printPowerPrices(double[][] prices) {
+        for (double[] row : prices) {
+            for (double value : row) {
+                System.out.print(value + " kr ");
+            }
+            System.out.println();
+        }
     }
 
-    // c) compute total power usage for a month
     public static double computePowerUsage(double[][] usage) {
-
         double sum = 0;
-
-        // TODO
-
+        for (double[] row : usage) {
+            for (double value : row) {
+                sum += value;
+            }
+        }
         return sum;
     }
 
-    // d) determine whether a given threshold in powerusage for the month has been exceeded
+    // d) determine whether a given threshold has been exceeded
     public static boolean exceedThreshold(double[][] powerusage, double threshold) {
+        double sum = 0; // threshold er parameter vi måler summen av månedlig forbruk mot,
+        int i = 0; // threshold får en verdi når vi kjører metoden exceedThreshold
+        boolean exceeded = false; // lager påstanden exceeded(overskredet) og sitter denne-
+// til å være feil, som betyr ikke overskredet, summen av månedlig forbruk er alle verdiene til
+// powerusage lagt sammen i while løkken
+        while (i < powerusage.length && !exceeded) { // kjører løkken helt til alle plassene i -
+            // tabellen er telt med og ikke exceeded som betyr ikke overskredet eller exceeded lik-,
+            int j = 0; // false
+            while (j < powerusage[i].length && !exceeded) {
+                sum += powerusage[i][j]; // setter summen er lik verdien til j i rad i i tabellen -
+                //  + den forrige summen
+                if (sum > threshold) { // vist sum er større enn overskredet verdi endres betydningen -
+                    exceeded = true; // av exceeded til sant, dette innebærer løkken stopper da -
+                } // påstanden exceeded er sann og betingelsen for at løkken skal kjøre ikke er -
+                j++; // oppfylt lengre, legger til både i og j så lenge løkken kjører
+            }
+            i++;
+        }
 
-        boolean exceeded = false;
-        double usage = 0;
+        if (exceeded) { // vist summen av alle forbrukene er overskredet print forbruket overstiger
+            // grensen + det totale forbruket
+            System.out.println("Forbruket overstiger grensen! (" + sum + " kWh)");
+        } else { // eller print forbruk er innenfor + det totale forbruket
+            System.out.println("Forbruket er innenfor grensen. (" + sum + " kWh)");
+        }
 
-        // TODO
-
-        return exceeded;
+        return exceeded; // returnerer overskredet verdi vist summen av forbruker overstiger grensen
     }
 
-    // e) compute spot price
     public static double computeSpotPrice(double[][] usage, double[][] prices) {
-
-        double price = 0;
-
-        // TODO
-
-        return price;
+        double total = 0; // i og j er begge knyttet(peker) opp til begge tabellene usage og -
+        // prices, noe som at løkken går gjennom begge tabellene for plassene og radene j og i,
+        for (int i = 0; i < usage.length; i++) { // kjører løkke som går gjennom radene
+            for (int j = 0; j < usage[i].length; j++) { // kjører løkke som går gjennom plassene
+                total += usage[i][j] * prices[i][j]; // ganger verdi til plass j i rad i til
+                // tabellen usage med plassen j i rad i til tabellen prices
+            }
+        }
+        return total;
     }
-
     // f) power support for the month
     public static double computePowerSupport(double[][] usage, double[][] prices) {
 
@@ -69,3 +92,5 @@ public class MonthlyPower {
         return price;
     }
 }
+
+
